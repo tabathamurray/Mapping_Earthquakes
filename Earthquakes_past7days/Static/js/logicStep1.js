ff515798c849d90df413817c3354bb30b09351a2
tabathamurray <107326497+tabathamurray@users.noreply.github.com>
2022-09-12 22:36:40 -0500

    Add files via upload

--- Earthquakes_past7days/Static/js/logicStep1.js
+++ Earthquakes_past7days/Static/js/logicStep1.js
@@ -23,9 +23,9 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [satelliteStreets]
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -35,19 +35,14 @@ L.control.layers(baseMaps).addTo(map);
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/tabathamurray/Mapping_Earthquakes/main/torontoNeighborhoods.json";
-
 // Create a style for the lines.
 let myStyle = {
   color: "#ffffa1",
   weight: 1
 }
 
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data,).addTo(map);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
 });
-
